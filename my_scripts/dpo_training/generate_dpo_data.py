011d103c5c7ce0622bb5f4b22fe0b6da8f7e886e
birdwithdreams <andr2004zur@gmail.com>
2024-12-12 18:09:04 +0000

    Updated 12.12

--- my_scripts/dpo_training/generate_dpo_data.py
+++ my_scripts/dpo_training/generate_dpo_data.py
@@ -1,3 +1,5 @@
+import random
+import traceback
 from itertools import product
 from pathlib import Path
 from uuid import uuid1
@@ -9,80 +11,121 @@ import soundfile as sf
 import pandas as pd
 import tqdm
 
+from loguru import logger
+
+from evaluate_tts import  compute_text_metrics, compute_audio_metric, pipe
+
 from TTS.api import TTS
+
+logger.add("../../logs/generate_dpo_data.log", rotation="10 MB", level="INFO")
 
 project_dir = Path(__file__).resolve().parents[2]
 default_prompt_path = project_dir / 'data' / 'dpo_texts_samples' / 'llama.csv'
-default_speakers_dir = project_dir / 'data' / 'speakers'
+# default_speakers_dir = project_dir / 'data' / 'speakers'
 default_model_path = project_dir / 'checkpoints' / 'last-lg'
 default_output_path = project_dir / 'data' / 'dpo_dataset'
+default_speakers_dir = default_output_path/ 'wavs'
 
+model_name = 'base_xtts_v2'
+model_path = project_dir
 
 @click.command()
 @click.option('--prompt-csv', default=default_prompt_path, help='Path to prompt CSV file')
 @click.option('--sample-size', default=5, help='Number of samples to generate')
 @click.option('--speakers-dir', default=default_speakers_dir, help='Path to speakers directory')
 @click.option('--output-folder', default=default_output_path, help='Name of output folder')
-def generate_tts(prompt_csv, sample_size, speakers_dir, output_folder):
-    prompts = pd.read_csv(prompt_csv, index_col=0).iloc[:10, 0]
-    model = TTS(
-        model_name='xtts_v2',
-        model_path=default_model_path,
-        config_path=default_model_path / 'config.json',
-    ).to('cuda')
+@click.option('--prompts-bounds', default='na,na', help='Name of output folder')
+@click.option('--output-name', default='dpo_data.parquet', help='Name of output folder')
+def generate_tts(prompt_csv, sample_size, speakers_dir, output_folder, prompts_bounds, output_name):
+    prompts = pd.read_csv(prompt_csv)
+
+    low, high = prompts_bounds.split(',')
+    if low == 'na':
+        low = 0
+    else:
+        low = int(low)
+
+    if high == 'na':
+        high = len(prompts)
+    else:
+        high = int(high)
+
+    prompts = prompts.iloc[low:high, 0]
+
+    if model_name == 'base_xtts_v2':
+        model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to('cuda')
+    else:
+        model = TTS(
+            model_name='xtts_v2',
+            model_path=model_path.as_posix(),
+            config_path=(model_path / 'config.json').as_posix(),
+        ).to('cuda')
 
     speakers_path = Path(speakers_dir)
     speakers = list(speakers_path.glob('*.wav'))
 
     output_path = Path(output_folder)
     output_path.mkdir(parents=True, exist_ok=True)
-    (output_path / 'wav').mkdir(parents=True, exist_ok=True)
+    # (output_path / 'wavs').mkdir(parents=True, exist_ok=True)
 
     metadata = []
 
-    for (id_, prompt), speaker, gen_i in tqdm.tqdm(
-            product(
+    for i, ((id_, prompt), speaker, gen_i) in tqdm.tqdm(
+            enumerate(product(
                 prompts.items(),
-                speakers,
+                speakers[:1],
                 range(sample_size)
-            ),
-            total=len(prompts) * len(speakers) * sample_size
+            )),
+            total=len(prompts) * len(speakers[:1]) * sample_size
     ):
-        speaker_name = speaker.name.split('.')[0]
+        try:
+            if gen_i >= 5:
+                speaker = random.choice(speakers)
 
-        # model.tts_to_file(
-        #     text=prompt['prompt'],
-        #     speaker_wav=speaker,
-        #     language="en",
-        #     file_path=output_path / f"({speaker_name})_{id_}_{gen_i}.wav"
-        # )
+            speaker_name = speaker.name.split('.')[0]
 
-        wav, gpt_codes = model.tts(
-            text=prompt,
-            speaker_wav=speaker,
-            language="en",
-        )
-        output_audio_name = f'{str(uuid1())}.wav'
+            wav, gpt_codes = model.tts(
+                text=prompt,
+                speaker_wav=speaker,
+                language="en",
+            )
 
-        sf.write(
-            output_path/'wav'/output_audio_name,
-            wav,
-            samplerate=model.config.audio["output_sample_rate"],
-        )
+            wav = np.array(wav, dtype=np.float32)
 
-        metadata.append(
-            {
-                'gen_id': str(gen_i),
-                'audio_id': output_audio_name,
-                'speaker_id': speaker_name,
-                'prompt_id': id_,
-                'text': prompt,
-                'gpt_codes': np.array(gpt_codes).flatten(),
-            }
-        )
+            transcription = pipe(wav)
+            transcription = transcription['text']
+
+            text_metrics = compute_text_metrics(prompt, transcription)
+            secs_metric, utmos_metric = compute_audio_metric((wav, 22050), speaker.name, default_output_path)
+
+            gpt_codes = np.hstack(gpt_codes)
+
+            metadata.append(
+                {
+                    'gen_id': str(gen_i),
+                    'audio_id': speaker.name,
+                    'speaker_id': speaker_name,
+                    'prompt_id': id_,
+                    'text': prompt,
+                    'gpt_codes': np.array(gpt_codes).flatten(),
+                } |
+                text_metrics |
+                {
+                    'secs': secs_metric,
+                    'utmos': utmos_metric,
+                }
+            )
+        except Exception as e:
+            logger.error(f'Exception {str(e)} was occurred while processing {speaker.name} with {prompt} prompt')
+            traceback.print_exc()
+
+        if (i +1)%100 == 0:
+            metadata = pd.DataFrame(metadata)
+            metadata.to_parquet(output_path/f'batch_{i}.parquet', engine='pyarrow')
+            metadata = []
 
     metadata = pd.DataFrame(metadata)
-    metadata.to_parquet(output_path/'samples_data.parquet', engine='pyarrow')
+    metadata.to_parquet(output_path/output_name, engine='pyarrow')
 
 
 if __name__ == '__main__':
